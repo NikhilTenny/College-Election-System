3a9e6583e6e81422d8bbee569c534ec28597d062
Nikhil <nikhiltenny19@gmail.com>
2021-09-11 14:57:00 +0530

    Landing,login,signup done. js also

--- js/landing.js
+++ js/landing.js
@@ -1,38 +1,49 @@
-
-function play_login(ele) {
-	if (ele.id == 'admin-opt')
-		document.getElementById('admin-opt').style.display = "none";
-
-	/*Remove text and lets go button */
-	if (document.getElementById("js_button").style.display !== "none")
-		document.getElementById("js_button").style.display = "none";
-	/*Remove sign up section*/
-	if (document.getElementById("Sign-up-main").style.display = "block")
-		document.getElementById("Sign-up-main").style.display = "none";
-	/*Display login section*/
-	if (document.getElementById("js_login_section").style.display === "none")
-		document.getElementById("js_login_section").style.display = "block";
-	/*Display the image*/
-	if (document.getElementById("vote-img-id").style.display !== "block")
-		document.getElementById("vote-img-id").style.display = "block";
-	/* Display the sign up option in nav bar*/
-	document.getElementById("sign-up-opt").style.removeProperty("display");	
+// remove everything except logo
+function remove_sections() {
+	let sign_up = document.getElementById('Sign-up-main');
+	let ad_log = document.getElementById('admin_login_form');
+	let stu_log = document.getElementById('student_login_form');
+	let text_img = document.getElementById('Front-text-img');
+	// Remove the style attribute from all the items in nav bar
+	// i.e to display it
+	try {
+	document.getElementById('sign-up-opt').removeAttribute("style");
+	document.getElementById('log-in-opt').removeAttribute("style");
+	document.getElementById('admin-opt').removeAttribute("style");}
+	catch(err) {
 	}
-
-function play_signup() {
-	/*Remove text and lets go button */
-	if (document.getElementById("js_button").style.display !== "none")
-		document.getElementById("js_button").style.display = "none";
-	/*Remove the image*/
-	if (document.getElementById("vote-img-id").style.display !== "none")
-		document.getElementById("vote-img-id").style.display = "none";
-	/*Remove the login section if present*/
-	if (document.getElementById("js_login_section").style.display !== "none")
-		document.getElementById("js_login_section").style.display = "none";
 	
-	document.getElementById("Sign-up-main").style.display = "block";
-	document.getElementById("sign-up-opt").style.display = "none";
-
-
+	if(sign_up.style.display == "block" ) { 
+		sign_up.style.display = "none";
+	}
+	if(ad_log.style.display == "block") {
+		ad_log.style.display = "none";
+	}
+	if(stu_log.style.display == "block") {
+		stu_log.style.display = "none";
+	}
+	if(text_img.style.display != ("block" || "none")) {
+		text_img.style.display = "none";
+	}
 }
 
+/*Display Student login section*/
+function print_stu_log() {
+	remove_sections();
+	document.getElementById('student_login_form').style.display = "block";
+	document.getElementById('log-in-opt').style.display = "None";
+}
+
+// Display sign up section
+function print_sign_up() {
+	remove_sections();
+	document.getElementById('Sign-up-main').style.display = "block";
+	document.getElementById('sign-up-opt').style.display = "None";
+}
+
+// Print admin login section
+function print_ad_log() {
+	remove_sections();
+	document.getElementById('admin_login_form').style.display = "block";
+	document.getElementById('admin-opt').style.display = "None";
+}
